bafe2901b71e34d3e39269d4ad32b7d3f66ddee3
lrw998 <betterwanglu@sina.com>
2020-06-01 15:49:17 +0800

    first commit

---
+++ mysqrt.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<cmath>
+
+#define eps 1e-6 //精度
+
+using namespace std;
+
+struct sqrt_st
+{
+    float result;
+    long runtime;
+};
+
+long getTimeNs()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME,&ts);
+
+    return ts.tv_sec*1000000000+ts.tv_nsec;
+}
+
+/*
+ * function:二分法实现sqrt()
+ * author:wanglu
+ */
+float mysqrt_1(float n)
+{
+    float left = 0, right = n;
+    float mid = 0;
+    float last;// 保留上一次的结果
+
+    if(n == 1) return 1;// 特判
+    if(n < 0) return 0;// 特判
+
+    do{
+
+        if(mid  > n / mid)// 避免溢出
+            right = mid;
+        else
+            left  = mid;
+
+        last = mid;
+
+        mid = left + (right-left) / 2;// 相较(left + right)/2 能避免溢出
+
+    }while(abs(mid-last) > eps);// 相比使用right-left > eps判断，这样更加精确
+
+    return mid;
+}
+
+/*
+ * function:牛顿迭代法实现sqrt()
+ * author:wanglu
+ */
+float mysqrt_2(float n)
+{
+    float init_value = n;// 牛顿法需要选择一个初始值，这里使等于n
+    float x = init_value;// return value
+    float last;// 保留上一次的结果
+
+    do{
+
+        last = x;
+        x = (x + n/x)/2;
+
+    }while(abs(x - last) > eps);// 比abs(x-n/x)>eps更精确
+
+    return x;
+}
+
+/*
+ * function:神奇的算法实现sqrt()
+ * author:wanglu
+ */
+float mysqrt_3(float x)
+{
+
+    float xhalf = 0.5f*x;
+    int i = *(int*)&x; 
+    
+    if(!x) return 0;
+    
+    i = 0x5f375a86- (i>>1); // beautiful number
+    x = *(float*)&i; 
+    x = x*(1.5f-xhalf*x*x); // 牛顿迭代法，提高精度
+    x = x*(1.5f-xhalf*x*x); // 牛顿迭代法，提高精度
+    x = x*(1.5f-xhalf*x*x); // 牛顿迭代法，提高精度
+
+    return 1/x;
+}
+
+struct sqrt_st test(float (*mysqrt)(float), float n)
+{
+    struct sqrt_st sqrt_struct;
+    
+    long start_time = getTimeNs();
+    sqrt_struct.result = mysqrt(n);
+    long end_time = getTimeNs();
+
+    sqrt_struct.runtime = end_time - start_time;
+
+    return sqrt_struct;
+}
+
+int main(int argc, char* argv[])
+{
+    
+    if(argc < 2)
+    {
+        std::cout<<"Less parameters\n";
+        exit(1);
+    }
+    float n = atof(argv[1]);
+
+
+    long start_time = getTimeNs();
+
+    // float result = mysqrt_2(n);
+    float result = sqrt(n);
+
+    long end_time = getTimeNs();
+
+ 
+
+    struct sqrt_st sqrt_st_1 = test(mysqrt_1,n);
+    std::cout<<"mtsqrt_1's result:"<<sqrt_st_1.result<<std::endl;
+    std::cout<<"mtsqrt_1's run time:"<<sqrt_st_1.runtime<<std::endl<<std::endl;
+
+
+    struct sqrt_st sqrt_st_2 = test(mysqrt_2,n);
+    std::cout<<"mtsqrt_2's result:"<<sqrt_st_2.result<<std::endl;
+    std::cout<<"mtsqrt_2's run time:"<<sqrt_st_2.runtime<<std::endl<<std::endl;
+
+    struct sqrt_st sqrt_st_3 = test(mysqrt_3,n);
+    std::cout<<"mtsqrt_3's result:"<<sqrt_st_3.result<<std::endl;
+    std::cout<<"mtsqrt_3's run time:"<<sqrt_st_3.runtime<<std::endl<<std::endl;
+
+    return 0;
+}
